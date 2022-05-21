ba9a27048cc91bc095c03f22f07951a74fdd08a4
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-05-20 23:26:57 -0700

    fixed typos in primer files

--- Notes/primer.js
+++ Notes/primer.js
@@ -6,31 +6,33 @@
 
 // 1. Define an `addFavoriteBook(..)` function that receives a single parameter, called`bookName`.
 
-// 2. If the provided `bookName` string does * NOT * have the word "Great" in it, add it to the `favoriteBooks` array.
+// 2. If the provided `bookName` string does * NOT * have the word "Great" in it, 
+// add it to the `favoriteBooks` array.
 
-// `someString.includes(anotherString)` will return `true` if `anotherString` is found inside`someString`, 
-// or`false` otherwise.
+// Use `!` to negate a boolean value(change `true` to `false` or vice versa).
 
-// Use`!` to negate a boolean value(change`true` to`false` or vice versa).
+// `someString.includes(anotherString)` will return `true` if `anotherString` is found 
+// inside `someString`, or `false` otherwise.
 
 // `someArray.push(value)` will add a value to the end of the array.
 
 // 3. Define a `printFavoriteBooks()` function that receives no parameters.
 
-// 4. `printFavoriteBooks()` should first print a message like "Favorite Books: ..", and include the number of books 
-// in the`favoriteBooks` array.
+// 4. `printFavoriteBooks()` should first print a message like "Favorite Books: ..", 
+// and include the number of books in the `favoriteBooks` array.
 
-// Use the \` back-tick operators for strings that need to include values in them.
+// Use the (``) back-tick operators for strings that need to include values in them.
 
-// Use `console.log(..)` to print a message to the screen.
-
-// 5. Finally, `printFavoriteBooks()` should loop through the `favoriteBooks` array and print out each value.
-
-// Make sure to then call the `printFavoriteBooks()` function at the end of the program.
+// 5. Finally, `printFavoriteBooks()` should loop through the `favoriteBooks` array 
+// and print out each value.
 
 // Hint: Use the `for (let ..of .. ) { }` style loop.
 
-// TODO: define addFavoriteBook(..) function'
+// Use `console.log(..)` to print a message to the screen.
+
+// Make sure to then call the `printFavoriteBooks()` function at the end of the program.
+
+// TODO: define addFavoriteBook(..) function
 function addFavoriteBook(bookName) {
     if (!bookName.includes("Great")) {
         favoriteBooks.push(bookName)
--- Notes/primerAns.js
+++ Notes/primerAns.js
@@ -3,12 +3,13 @@
 // This is an exercise to practice basic programming principles.
 
 // TODO: define addFavoriteBook(..) function
-// 1. Define an `addFavoriteBook(..)` function that receives a single parameter, called`bookName`.
+// 1. Define an `addFavoriteBook(..)` function that receives a single parameter, called `bookName`.
 function addFavoriteBook(bookName) {
-    // 2. If the provided `bookName` string does * NOT * have the word "Great" in it, add it to the `favoriteBooks` array.
-    // 	Use`!` to negate a boolean value(change`true` to`false` or vice versa).
-    // `someString.includes(anotherString)` will return `true` if `anotherString` is found inside`someString`, or`false` 
-    // otherwise.
+    // 2. If the provided `bookName` string does * NOT * have the word "Great" in it, 
+    // add it to the `favoriteBooks` array.
+    // Use `!` to negate a boolean value(change `true` to `false` or vice versa).
+    // `someString.includes(anotherString)` will return `true` if `anotherString` is found 
+    // inside `someString`, or `false` otherwise.
     if (!bookName.includes("Great")) {
         // `someArray.push(value)` will add a value to the end of the array.
         favoriteBooks.push(bookName)
@@ -18,14 +19,15 @@ function addFavoriteBook(bookName) {
 // TODO: define printFavoriteBooks() function
 // 3. Define a `printFavoriteBooks()` function that receives no parameters.
 function printFavoriteBooks() {
-    // 4. `printFavoriteBooks()` should first print a message like "Favorite Books: ..", and include the number of books 
-    // in the`favoriteBooks` array.
-    // Use the \` back-tick operators for strings that need to include values in them.
-    // Use `console.log(..)` to print a message to the screen.
+    // 4. `printFavoriteBooks()` should first print a message like "Favorite Books: ..", 
+    // and include the number of books in the `favoriteBooks` array.
+    // Use the (``) back-tick operators for strings that need to include values in them.
     console.log(`Favorite books: ${favoriteBooks.length}`)
-    // 5. Finally, `printFavoriteBooks()` should loop through the `favoriteBooks` array and print out each value.
+    // 5. Finally, `printFavoriteBooks()` should loop through the `favoriteBooks` array 
+    // and print out each value.
     // Hint: Use the `for (let ..of .. ) { }` style loop.
     for (let bookName of favoriteBooks) {
+        // Use `console.log(..)` to print a message to the screen.
         console.log(bookName)
     }
 }
@@ -39,5 +41,5 @@ addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 // TODO: print out favorite books
-// 	Make sure to then call the `printFavoriteBooks()` function at the end of the program.
+// Make sure to then call the `printFavoriteBooks()` function at the end of the program.
 printFavoriteBooks()
